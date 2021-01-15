3b56df9804f52f3517156f5c529c2dd12b7cd9a6
Ethan Shook <eshook89@gmail.com>
2021-01-14 19:52:59 -0600

    added app.js

--- wint-ipsum/src/App.js
+++ wint-ipsum/src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const express = require('express');
+const app = express();
+const routes = require('./router');
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://${hostname}:${port}/`);
+})
 
 export default App;

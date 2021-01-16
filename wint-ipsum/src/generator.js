394d9627fa1c973de2b48fe0c94307124d8d0571
Ethan Shook <eshook89@gmail.com>
2021-01-16 13:33:00 -0600

    added strings for text generation

--- wint-ipsum/src/generator.js
+++ wint-ipsum/src/generator.js
@@ -8,10 +8,16 @@ function GenerateNewText() {
   // Add property to the object
   this.sentences =
     [
-      "Awesome quote number 1.",
-      "Second awesome quote.",
-      "Yet another great quote.",
-      "Ok, last quote."
+      `how do i get cowboy paint off a dog .`,
+      `im little jesica. im dying because of obamas help care bill. im on my death bed and the doctor is ignoring me because my dady works hard`,
+      `Welcome to the citadel of eternal wisdom. Behold, this crystal contains the sum of all human knowledge -- Except Rap And Country`,
+      `icant come to work today.. on account of JERRY DUTY *SHoves every seinfeld disk into dvd player at once*`,
+      `'im not owned! im not owned!!', i continue to insist as i slowly shrink and transform into a corn cob`,
+      `fuck "jokes". everything i tweet is real. raw insight without the horse shit. no, i will NOT follow trolls. twitter dot com. i live for this`,
+      `see this watch? i got it by Crying. my car? crying. my beautiful wife? Crying. My perfect teeth? Crying. now get the fuck out of my office`,
+      `another day volunteering at the betsy ross museum. everyone keeps asking me if they can fuck the flag. buddy, they wont even let me fuck it`,
+      `how come a baby born with a foot in its brain is considered a "Miracle Baby" but when I get my dick stuck in a drawer im just some asshole`,
+      `"jail isnt real," i assure myself as i close my eyes and ram the hallmark gift shop with my shitty bronco`,
    ];
 }
 
